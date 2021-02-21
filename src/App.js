74b792d9f16b5de72da0a7b42414c68c0c22c8d0
Poornima Natikar <poorniman36@gmail.com>
2021-02-21 13:38:47 +0530

    initital styling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  
+  return (
+  <div className={styles.main}>
+      <h2>COUNTRY QUIZ</h2>
+      <div className={styles.quiz}>
+      </div>
+  </div>
+  );
+}
+export default App;
